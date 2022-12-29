9f00316d3f7e5d6faba07d944e14a8a4804b2827
Amenofisch <51821196+Amenofisch@users.noreply.github.com>
2022-12-29 17:45:59 +0100

    changed export of config

--- config/config.js
+++ config/config.js
@@ -1,16 +1,18 @@
 var Ledstrip = require('../modules/ledstrip.mod.js');
 
-module.exports = class Config {
+class Config {
     static server = {
         name: "ELK-BLEDOM-RECODE",
         version: "v1.0.0",
         port: 5000
     };
     static bluetooth = {
-        device: "hci0",
+        device: "hci0"
     }
     static devices = [
         new Ledstrip("ELK-BLEDOM 1", "BE:FF:20:00:06:FF", "0x0008"),
         new Ledstrip("ELK-BLEDOM 2", "BE:59:0D:00:16:DB", "0x0009"),
     ]
 }
+
+module.exports = { Config }; 
