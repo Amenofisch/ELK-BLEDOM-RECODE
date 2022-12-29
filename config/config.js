af790327876b4640584b3859cde0f6c157f28646
Amenofisch <51821196+Amenofisch@users.noreply.github.com>
2022-12-29 23:11:42 +0100

    removed unnecessary module and added some docs to config.js

--- config/config.js
+++ config/config.js
@@ -1,14 +1,16 @@
 var Ledstrip = require('../modules/ledstrip.mod.js');
 
 module.exports = class Config {
+    // Change these settings if you have multiple instances of this server
     static server = {
         name: "ELK-BLEDOM-RECODE",
-        version: "v1.0.0",
+        version: "v1.0.0", // Do not change this
         port: 5000
     };
     static bluetooth = {
         device: "hci0"
     }
+    // You can add as many LED strips as you want here
     static devices = [
         new Ledstrip("ELK-BLEDOM 1", "BE:FF:20:00:06:FF", "0x0008", this.bluetooth.device),
         new Ledstrip("ELK-BLEDOM 2", "BE:59:0D:00:16:DB", "0x0009", this.bluetooth.device),
