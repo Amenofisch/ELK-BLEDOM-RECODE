f9bdad9cb67229e442b85ff478e755848f81d327
Amenofisch <51821196+Amenofisch@users.noreply.github.com>
2022-12-29 18:04:33 +0100

    added routes to controll all devices at once

--- routes/ledstrip.route.js
+++ routes/ledstrip.route.js
@@ -2,14 +2,56 @@ var express = require('express');
 var router = express.Router();
 var config = require('../config/config');
 
+// This route returns the config of all devices
 router.get('/', function(req, res, next) {
     res.send(config.devices);
 });
 
+// This route returns the config of a single device
 router.get('/:id', function(req, res, next) {
     res.send(config.devices[req.params.id]);
 });
 
+// This route controls the power of all devices
+router.post('/power', function(req, res, next) {
+    if(req.body.value == undefined) return res.status(400).send("No value specified");
+    let value = req.body.value;
+    if(typeof value != "boolean") return res.status(400).send("Value must be a boolean");
+
+    let resp = [];
+    for(let i = 0; i < config.devices.length; i++) {
+        resp.push(config.devices[i].setPower(value));
+    }
+    res.send(resp);
+})
+
+// This route controls the brightness of all devices
+router.post('/brightness', function(req, res, next) {
+    if(req.body.value == undefined) return res.status(400).send("No value specified");
+    let value = req.body.value;
+    if(typeof value != "number") return res.status(400).send("Value must be a number");
+
+    let resp = [];
+    for(let i = 0; i < config.devices.length; i++) {
+        resp.push(config.devices[i].setBrightness(value));
+    }
+    res.send(resp);
+})
+
+// This route controls the color of all devices
+router.post('/color', function(req, res, next) {
+    if(req.body.value == undefined) return res.status(400).send("No value specified");
+    let value = req.body.value;
+    if(typeof value != "string") return res.status(400).send("Value must be a string");
+
+    let resp = [];
+    for(let i = 0; i < config.devices.length; i++) {
+        resp.push(config.devices[i].setColor(value.toLowerCase()));
+    }
+    res.send(resp);
+})
+
+// This route controls the power of a single device
 router.post('/power/:id', function(req, res, next) {
     if(req.body.value == undefined) return res.status(400).send("No value specified");
     let value = req.body.value;
@@ -20,6 +62,7 @@ router.post('/power/:id', function(req, res, next) {
     res.send(resp);
 });
 
+// This route controls the brightness of a single device
 router.post('/brightness/:id', function(req, res, next) {
     if(req.body.value == undefined) return res.status(400).send("No value specified");
     let value = req.body.value;
@@ -30,6 +73,7 @@ router.post('/brightness/:id', function(req, res, next) {
     res.send(resp);
 });
 
+// This route controls the color of a single device
 router.post('/color/:id', function(req, res, next) {
     if(req.body.value == undefined) return res.status(400).send("No value specified");
     let value = req.body.value;
