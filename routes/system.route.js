eee86d8e99f5897e5320a69a61b0f50da3b201d2
Amenofisch <51821196+Amenofisch@users.noreply.github.com>
2023-01-01 23:15:41 +0100

    stringiefied config route

--- routes/system.route.js
+++ routes/system.route.js
@@ -1,10 +1,11 @@
+const { json } = require('express');
 var express = require('express');
 var router = express.Router();
 var config = require('../config/config');
 
 // This route returns the config of the server
 router.get('/config', function(req, res, next) {
-    res.send(config);
+    res.send(JSON.stringify(config));
 });
 
 
