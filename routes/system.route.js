4842dfe7d6669539257f42e713d37df4d40eab54
Amenofisch <51821196+Amenofisch@users.noreply.github.com>
2023-01-04 23:11:27 +0100

    implemented config route; implemented proper searching for device with id

--- routes/system.route.js
+++ routes/system.route.js
@@ -6,17 +6,20 @@ var config = require('../config/config');
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
-// This route returns the config of the server
-router.get('/config', function(req, res, next) {
-    res.send(config);
-});
-
 router.get('/config/devices', function(req, res, next) {
     res.send(config.devices);
 });
 
 router.get('/config/devices/:id', function(req, res, next) {
-    res.send(config.devices[req.params.id]);
+    res.send(config.devices.where(d => d.id == req.params.id));
 });
+
+router.get('/config', function(req, res, next) {
+    res.send({
+        server: config.server,
+        bluetooth: config.bluetooth,
+        devices: config.devices
+    });
+})
 
 module.exports = router;
